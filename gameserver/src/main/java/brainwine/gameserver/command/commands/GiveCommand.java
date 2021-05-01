b3e89c9ad0b8b31aa0f48531c4e9ed4c019ce536
Sir Code <taylorschneider07@yahoo.com>
2021-05-01 15:02:52 -0700

    Updated command + Added ability to subtract / add specific amounts of all items

--- gameserver/src/main/java/brainwine/gameserver/command/commands/GiveCommand.java
+++ gameserver/src/main/java/brainwine/gameserver/command/commands/GiveCommand.java
@@ -25,42 +25,52 @@ public class GiveCommand extends Command {
         
         Item item = Item.AIR;
         
-        if (args[1].equalsIgnoreCase("all")) {
-            for(Item curItem : ItemRegistry.getItems()) {
-            	target.getInventory().addItem(curItem, 9999);
-            }
-	    } else {        	
-        	try {
-        		item = ItemRegistry.getItem(Integer.parseInt(args[1]));
-        	} catch(NumberFormatException e) {
-        		item = ItemRegistry.getItem(args[1]);
-        	}
+    	try {
+    		item = ItemRegistry.getItem(Integer.parseInt(args[1]));
+    	} catch(NumberFormatException e) {
+    		item = ItemRegistry.getItem(args[1]);
+    	}
 
-            if(item.isAir()) {
-                executor.sendMessage("This item does not exist.");
+        if(item.isAir() && !args[1].equalsIgnoreCase("all")) {
+            executor.sendMessage("This item does not exist.");
+            return;
+        }
+        
+        int quantity = 1;
+        
+        if(args.length > 2) {
+            try {
+                quantity = Integer.parseInt(args[2]);
+            } catch(NumberFormatException e) {
+                executor.sendMessage("Quantity must be a valid number.");
                 return;
             }
-            
-            int quantity = 1;
-            
-            if(args.length > 2) {
-                try {
-                    quantity = Integer.parseInt(args[2]);
-                } catch(NumberFormatException e) {
-                    executor.sendMessage("Quantity must be a valid number.");
-                    return;
+        }
+        
+        if(quantity > 0) {
+        	if (args[1].equalsIgnoreCase("all")) {
+                for(Item curItem : ItemRegistry.getItems()) {
+                	target.getInventory().addItem(curItem, quantity);
+                    target.alert(String.format("You received %s of every item from an administrator.", quantity));
+                    executor.sendMessage(String.format("Gave %s of every item to %s", quantity, target.getName()));
                 }
-            }
-            
-            if(quantity > 0) {
+        	} else {
                 target.getInventory().addItem(item, quantity);
                 target.alert(String.format("You received %s %s from an administrator.", quantity, item.getTitle()));
                 executor.sendMessage(String.format("Gave %s %s to %s", quantity, item.getTitle(), target.getName()));
-            } else {
+        	}
+        } else {
+        	if (args[1].equalsIgnoreCase("all")) {
+                for(Item curItem : ItemRegistry.getItems()) {
+                	target.getInventory().removeItem(curItem, -quantity);
+                    target.alert(String.format("%s of all items were taken from your inventory.", -quantity));
+                    executor.sendMessage(String.format("Took %s of all items from %s", quantity, target.getName()));
+                }
+        	} else {
                 target.getInventory().removeItem(item, -quantity);
                 target.alert(String.format("%s %s was taken from your inventory.", -quantity, item.getTitle()));
                 executor.sendMessage(String.format("Took %s %s from %s", quantity, item.getTitle(), target.getName()));
-            }
+        	}
         }
         
     }
