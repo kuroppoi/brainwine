83e6230b888103a34e51f2a7c186e7704239a823
kuroppoi <68156848+kuroppoi@users.noreply.github.com>
2024-02-04 23:11:29 +0100

    Remove note

--- gameserver/src/main/java/brainwine/gameserver/item/consumables/Consumable.java
+++ gameserver/src/main/java/brainwine/gameserver/item/consumables/Consumable.java
@@ -3,7 +3,6 @@ package brainwine.gameserver.item.consumables;
 import brainwine.gameserver.entity.player.Player;
 import brainwine.gameserver.item.Item;
 
-// TODO properly handle dialog cancellation for v2 clients
 public interface Consumable {
     
     public void consume(Item item, Player player, Object details);
