1466ea0176a4e473afb90388a33d0d95075ade7a
kuroppoi <68156848+kuroppoi@users.noreply.github.com>
2024-02-24 00:46:23 +0100

    Fix player name inconsistency on v2 clients

--- api/src/main/java/brainwine/api/DataFetcher.java
+++ api/src/main/java/brainwine/api/DataFetcher.java
@@ -9,6 +9,7 @@ public interface DataFetcher {
     public boolean isPlayerNameTaken(String name);
     public String registerPlayer(String name);
     public String login(String name, String password);
+    public String fetchPlayerName(String name);
     public boolean verifyAuthToken(String name, String token);
     public boolean verifyApiToken(String apiToken);
     public Collection<ZoneInfo> fetchZoneInfo();
