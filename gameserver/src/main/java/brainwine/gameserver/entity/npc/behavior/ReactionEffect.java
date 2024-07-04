3caf33d4b8412da6e309ccc595e839bb3975d9cf
Mert Bora İnevi <mertborainevi@gmail.com>
2024-07-04 04:43:11 +0200

    add crafter job

--- gameserver/src/main/java/brainwine/gameserver/entity/npc/behavior/ReactionEffect.java
+++ gameserver/src/main/java/brainwine/gameserver/entity/npc/behavior/ReactionEffect.java
@@ -1,47 +1,86 @@
 package brainwine.gameserver.entity.npc.behavior;
 
 import java.util.List;
+import java.util.function.Predicate;
 
-import io.netty.util.internal.shaded.org.jctools.queues.MessagePassingQueue.Consumer;
-
+/**Represents an effect which the entity may react to.
+ * It infers the type of the effect sent from the client and calls the lambda passed to the correct handler.
+ * 
+ * This is mainly to handle cases of users clicking/tapping onto an android.
+ * 
+ * Look at the documentation of methods starting with on- to see what case they handle.
+ */
 public class ReactionEffect {
     private Type type;
     private Object data;
+    private boolean handled = false;
 
     private ReactionEffect(Type type, Object data) {
         this.type = type;
         this.data = data;
     }
 
-    private <T> ReactionEffect handle(Type type, Consumer<T> handler) {
+    @SuppressWarnings({ "", "unchecked" })
+    private <T> ReactionEffect handle(Type type, Predicate<T> handler) {
         if(this.type == type) {
-            try {
-                handler.accept((T)data);
-            }
-            catch(ClassCastException e) {}
+            handled = handled || handler.test((T)data);
         }
 
         return this;
+    }
+
+    public void clearHandled() {
+        handled = false;
+    }
+
+    public boolean isHandled() {
+        return handled;
     }
 
     // Add new types of effects below this line.
 
     private static enum Type {
         INTERACT,
+        DROP_ITEM_ONTO,
+        UNKNOWN,
     }
 
     public static ReactionEffect interact(Object data) {
-        List<Object> myData;
-        if (data instanceof List) {
-            myData = (List<Object>)data;
-        } else {
-            myData = List.of(data);
+        if(data instanceof List) {
+            List<Object> list = (List<Object>)data;
+
+            if(list.size() < 2) {
+                return interact(list.get(0));
+            }
+            
+            if("item".equals(list.get(0))) {
+                return new ReactionEffect(Type.DROP_ITEM_ONTO, list.get(1));
+            }
+        } else if(data instanceof Integer) {
+            return new ReactionEffect(Type.INTERACT, data);
         }
-        return new ReactionEffect(Type.INTERACT, myData);
+
+        return new ReactionEffect(Type.UNKNOWN, data);
     }
 
-    public ReactionEffect onInteract(Consumer<List<Integer>> fn) {
+    /**
+     * When the user taps onto the android
+     * 
+     * @param fn callback function. Return true iff the interaction is wholly handled.
+     * @return itself
+     */
+    public ReactionEffect onInteract(Predicate<Integer> fn) {
         return handle(Type.INTERACT, fn);
+    }
+
+    /**
+     * When the user drags an item onto the android
+     * 
+     * @param fn callback function. Return true iff the interaction is wholly handled.
+     * @return itself
+     */
+    public ReactionEffect onDropItemOnto(Predicate<Integer> fn) {
+        return handle(Type.DROP_ITEM_ONTO, fn);
     }
 
 }
