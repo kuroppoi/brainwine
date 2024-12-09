1576360f56af554ce0b827b63f5209d48eba1ab0
Mert Bora İnevi <mertborainevi@gmail.com>
2024-12-09 01:56:44 +0100

    do not spawn rubble on teleporters

--- gameserver/src/main/java/brainwine/gameserver/zone/Zone.java
+++ gameserver/src/main/java/brainwine/gameserver/zone/Zone.java
@@ -751,7 +751,8 @@ public class Zone {
                     }
                     
                     // Try to place rubble
-                    if(decay && frontItem.isWhole() && !isBlockOccupied(x + i, y + j - 1, Layer.FRONT) && random.nextDouble() <= 0.2) {
+                    if(decay && frontItem.isWhole() && random.nextDouble() <= 0.2
+                            && findBlock(x + i, y + j - 1, b -> !b.getFrontItem().isAir()) == null) {
                         RubbleType[] types = RubbleType.values();
                         RubbleType type = types[random.nextInt(types.length)];
                         String[] itemIds = type.getItemIds();
