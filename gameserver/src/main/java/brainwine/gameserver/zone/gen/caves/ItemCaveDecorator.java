4cd9d407e2b906d1b9beabc2ecb42df1f1f1e22f
Mert Bora İnevi <mertborainevi@gmail.com>
2024-12-12 23:35:59 +0100

    direction of y axis

--- gameserver/src/main/java/brainwine/gameserver/zone/gen/caves/ItemCaveDecorator.java
+++ gameserver/src/main/java/brainwine/gameserver/zone/gen/caves/ItemCaveDecorator.java
@@ -96,8 +96,8 @@ public class ItemCaveDecorator extends CaveDecorator {
             // Clear anything that might cover up the item
             for(int i = 0; i < item.getBlockWidth(); i++) {
                 for(int j = 0; j < item.getBlockHeight(); j++) {
-                    if(ctx.isSolid(i + x, j + y)) {
-                        ctx.updateBlock(i + x, j + y, Layer.FRONT, Item.AIR);
+                    if(ctx.isSolid(i + x, j - y)) {
+                        ctx.updateBlock(i + x, j - y, Layer.FRONT, Item.AIR);
                     }
                 }
             }
