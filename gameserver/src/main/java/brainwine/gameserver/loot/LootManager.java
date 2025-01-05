4f0e2aa6319921726ee9af9200220c75ffe4684e
kuroppoi <68156848+kuroppoi@users.noreply.github.com>
2025-01-05 22:32:35 +0100

    Rework loot/luck system

--- gameserver/src/main/java/brainwine/gameserver/loot/LootManager.java
+++ gameserver/src/main/java/brainwine/gameserver/loot/LootManager.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
@@ -18,15 +19,18 @@ import org.apache.logging.log4j.Logger;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 
+import brainwine.gameserver.item.Item;
 import brainwine.gameserver.player.Player;
 import brainwine.gameserver.player.Skill;
 import brainwine.gameserver.resource.ResourceFinder;
 import brainwine.gameserver.util.WeightedMap;
+import brainwine.gameserver.zone.Biome;
 import brainwine.shared.JsonHelper;
 
 public class LootManager {
     
-    public static final int[] MIN_FREQUENCIES_BY_LUCK = {18, 15, 12, 9, 7, 5, 4, 3, 2};
+    public static final double LEVELS_PER_BONUS_ROLL = 6.0;
+    public static final int MAX_BONUS_ROLLS = 20;
     private static final Logger logger = LogManager.getLogger();
     private final Map<String, List<Loot>> lootTables = new HashMap<>();
     
@@ -50,20 +54,16 @@ public class LootManager {
         return lootTables.getOrDefault(category, Collections.emptyList());
     }
     
-    public List<Loot> getEligibleLoot(Player player, String... categories) {
-        return getEligibleLoot(player, Arrays.asList(categories));
+    public List<Loot> getEligibleLoot(Biome biome, Set<Item> ignore, String... categories) {
+        return getEligibleLoot(biome, ignore, Arrays.asList(categories));
     }
     
-    public List<Loot> getEligibleLoot(Player player, Collection<String> categories) {
-        int luck = player.getTotalSkillLevel(Skill.LUCK);
-        int minFrequency = luck > MIN_FREQUENCIES_BY_LUCK.length ? 1 : MIN_FREQUENCIES_BY_LUCK[luck - 1];
+    public List<Loot> getEligibleLoot(Biome biome, Set<Item> ignore, Collection<String> categories) {
         List<Loot> eligibleLoot = lootTables.entrySet().stream()
                 .filter(entry -> categories.contains(entry.getKey()))
                 .map(Entry::getValue)
                 .flatMap(Collection::stream)
-                .filter(loot -> (loot.getBiome() == null || loot.getBiome() == player.getZone().getBiome())
-                        && (Math.random() <= luck * 0.015 || loot.getFrequency() >= minFrequency)
-                        && !player.getInventory().getWardrobe().containsAll(loot.getItems().keySet()))
+                .filter(loot -> (loot.getBiome() == null || loot.getBiome() == biome) && !ignore.containsAll(loot.getItems().keySet()))
                 .collect(Collectors.toList());
         return eligibleLoot;
     }
@@ -73,6 +73,33 @@ public class LootManager {
     }
     
     public Loot getRandomLoot(Player player, Collection<String> categories) {
-        return new WeightedMap<>(getEligibleLoot(player, categories), Loot::getFrequency).next();
+        return getRandomLoot(player.getTotalSkillLevel(Skill.LUCK), player.getZone().getBiome(), player.getInventory().getWardrobe(), categories);
+    }
+    
+    public Loot getRandomLoot(int luck, Biome biome, Set<Item> ignore, String... categories) {
+        return getRandomLoot(luck, biome, ignore, Arrays.asList(categories));
+    }
+    
+    public Loot getRandomLoot(int luck, Biome biome, Set<Item> ignore, Collection<String> categories) {
+        WeightedMap<Loot> map = new WeightedMap<>(getEligibleLoot(biome, ignore, categories), Loot::getFrequency);
+        Loot loot = map.next();
+        double rolls = (luck - 1) / LEVELS_PER_BONUS_ROLL;
+        int bonusRolls = Math.min(MAX_BONUS_ROLLS, (int)rolls);
+        
+        // Turn remainder into a chance to get an extra bonus roll
+        if(bonusRolls < MAX_BONUS_ROLLS && Math.random() < (rolls - bonusRolls)) {
+            bonusRolls++;
+        }
+        
+        // Perform bonus rolls and return the lowest frequency loot
+        for(int i = 0; i < bonusRolls; i++) {
+            Loot next = map.next();
+            
+            if(loot == null || (next != null && next.getFrequency() < loot.getFrequency())) {
+                loot = next;
+            }
+        }
+        
+        return loot;
     }
 }
