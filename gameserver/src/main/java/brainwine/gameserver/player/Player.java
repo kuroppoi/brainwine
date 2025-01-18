e9b34759676472ec7c74e94f039f930a26815f0b
kuroppoi <68156848+kuroppoi@users.noreply.github.com>
2025-01-19 00:41:16 +0100

    Force player out of hell if they're unbanned

--- gameserver/src/main/java/brainwine/gameserver/player/Player.java
+++ gameserver/src/main/java/brainwine/gameserver/player/Player.java
@@ -1230,6 +1230,10 @@ public class Player extends Entity implements CommandExecutor {
         if(currentBan != null) {
             currentBan.pardon(issuer);
         }
+        
+        if(isOnline()) {
+            changeZone(null);
+        }
     }
     
     public boolean isBanned() {
