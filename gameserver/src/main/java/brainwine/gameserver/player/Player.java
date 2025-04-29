bf937dd967ee6adb770a0fadc1afa15beaa9a998
Mert Bora İnevi <mertborainevi@gmail.com>
2025-04-29 23:53:27 +0300

    send appearance when changing zones

--- gameserver/src/main/java/brainwine/gameserver/player/Player.java
+++ gameserver/src/main/java/brainwine/gameserver/player/Player.java
@@ -472,7 +472,7 @@ public class Player extends Entity implements CommandExecutor {
     public Map<String, Object> getStatusConfig() {
         Map<String, Object> config = super.getStatusConfig();
         config.put("id", documentId);
-        config.putAll(appearance);
+        config.putAll(getVisibleAppearance());
         config.put("u", inventory.findJetpack().getCode());
         config.put("ni", getIcon());
         return config;
@@ -554,6 +554,8 @@ public class Player extends Entity implements CommandExecutor {
         zone.sendMachineStatus(this);
         sendMessage(new PlayerPositionMessage((int)x, (int)y));
         sendMessage(new HealthMessage(health));
+        // Configuration message doesn't cause the Unity client to update the appearance when changing zones
+        sendMessage(new EntityChangeMessage(getId(), getVisibleAppearance()));
         
         // Send skill data
         for(Skill skill : skills.keySet()) {
